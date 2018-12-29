200d7d3798e3e089b2078ee312da648956519008
laichengfeng <1458428190@qq.com>
2018-12-29 16:26:15 +0800

    feat: XXL-job学习

--- xxl-job/xxl-job-demo/src/main/java/com/gdufe/study/xxljobdemo/config/XxlJobConfig.java
+++ xxl-job/xxl-job-demo/src/main/java/com/gdufe/study/xxljobdemo/config/XxlJobConfig.java
@@ -1,13 +1,12 @@
 package com.gdufe.study.xxljobdemo.config;
 
+import com.xxl.job.core.executor.XxlJobExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.xxl.job.core.executor.XxlJobExecutor;
 
 /**
  * xxl-job config
